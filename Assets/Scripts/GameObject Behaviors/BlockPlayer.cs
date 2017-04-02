819dcc90d40945eca949f33c956e8152c80eb113
Ryan-PC\Ryan <evert004@cougars.csusm.edu>
2017-04-01 19:58:49 -0700

    Added shield script so an object can hover in front of player. Added ShootPlayer script to make gameobjects fire at the player(note the GO must be at the same y level as the player to reflect correctly). I copied the fire code from the player controller for now. Also includes the scene I was using to test.

---
+++ Assets/Scripts/GameObject Behaviors/BlockPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlayer : MonoBehaviour {
+
+    public Vector3 offset;
+
+    private GameObject shield;
+    private GameObject parent;
+
+    private void Start()
+    {
+        parent = this.transform.parent.gameObject;
+        
+    }
+    private void Update()
+    {
+        this.transform.position = parent.transform.position;
+        this.transform.localPosition = offset;
+    }
+}
+
---
+++ Assets/Scripts/GameObject Behaviors/ShootPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPlayer : MonoBehaviour {
+
+    public float fireCooldown;
+
+    private GameObject player;
+
+    private float nextFire;
+
+    private GameObject dynamicsHolder;
+
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+        dynamicsHolder = GameObject.FindWithTag("DYNAMICS_HOLDER");
+    }
+	
+	void Update () {
+        this.transform.LookAt(player.transform);
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireCooldown;
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        Vector3 projectilePosition = transform.position;
+        Quaternion projectileRotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+        Instantiate(Resources.Load("LaserBolt"), projectilePosition, projectileRotation, dynamicsHolder.transform);
+    }
+}
