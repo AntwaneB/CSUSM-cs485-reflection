4729d403722ef036b8ae9c46045e1d143b254d21
Ryan-PC\Ryan <evert004@cougars.csusm.edu>
2017-05-03 16:38:43 -0700

    Added EMP, controlled by EMP controller. Can be connected to multiple turrets. Later I may want to make it damage turrets on its destruction so some turrets may require multiple EMPs to die. I also want to make them change color on hit so the player can tell how many times he has damaged them.

---
+++ Assets/Scripts/Controllers/EMPController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EventSystem;
+using System;
+
+public class EMPController : MonoBehaviour, EventListener {
+
+    public GameObject[] connectedTurrets; 
+
+	void Start () {
+        EventManager.get().Subscribe(this, new HashSet<Type>
+        {
+            typeof(UnitDiedEvent)
+        });
+    }
+	
+	void Update ()
+    {
+    }
+
+    public void OnNotify(EventSystem.Event e)
+    {
+        if (e is UnitDiedEvent)
+        {
+            UnitDiedEvent unitDiedEvent = e as UnitDiedEvent;
+
+            if (unitDiedEvent.GameObject == this.gameObject)
+            {
+                UnitDiedEvent unitDied = e as UnitDiedEvent;
+                foreach (GameObject turret in connectedTurrets)
+                {
+                    EventManager.get().Notify(new UnitDiedEvent(turret));
+                }
+                die();
+            }
+        }
+    }
+
+    private void die()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
